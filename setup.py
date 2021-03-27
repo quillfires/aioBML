247d6c0d2ed93bab2f4cf25e4b74938dc7f6ebb7
quillfires <48891838+quillfires@users.noreply.github.com>
2021-03-28 03:31:40 +0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python3
-import functools
 import pathlib
 import sys
 
@@ -19,34 +18,6 @@ if sys.version_info < MINIMAL_PY_VERSION:
 
 VERSION = '0.0.5'
 LONG_DESCRIPTION = "Its a simple asynchronous Python API wrapper that returns the transaction history of all your accounts from the Bank of Maldives web API. If you want to check for new transactions, save the transactions to a db, check and add any transactions that's not currently saved to the db."
-
-def get_description():
-    """
-    Read full description from 'README.md'
-
-    :return: description
-    :rtype: str
-    """
-    with open('README.md', 'r', encoding='utf-8') as f:
-        return f.read()
-
-
-@functools.lru_cache()
-def get_requirements(filename=None):
-    """
-    Read requirements from 'requirements txt'
-
-    :return: requirements
-    :rtype: list
-    """
-    if filename is None:
-        filename = 'requirements.txt'
-
-    file = WORK_DIR / filename
-
-    install_reqs = parse_requirements(str(file), session='hack')
-    return [str(ir) for ir in install_reqs]
-
 
 setup(
     name='aiobml',
